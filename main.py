0a189bb79f27667db7756acd86ad67dbbe5b8eb8
ren-lim <ren.lim@edu.esiee.fr>
2025-11-13 09:19:25 +0000

    rendu le 13/11/25

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+"""Notre objectif est de déterminer si un mot est un palyndrome ou non"""
+import string
 #### Fonction secondaire
-
-
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """Cette fonction nous permet de tester si notre mot en paramètre est un palyndrome"""
+    accent = "àâäéèêëîïôöûüùç"
+    sans_accent = "aaaeeeeiioouuuc"
+    suppr_espace = string.punctuation + " " + "’"
+    translation_table = str.maketrans(accent, sans_accent, suppr_espace)
+    p_minuscule = p.lower().translate(translation_table)
+    return p_minuscule == p_minuscule[::-1]
 
 #### Fonction principale
-
-
 def main():
-
+    """C'est notre  fonction principale qui teste ispalindrome()"""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
